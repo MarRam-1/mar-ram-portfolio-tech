5b235e7c7e1a3d77a95a563106875a3ea8ad9a62
MarRam-1 <mariana.rmrz.96@gmail.com>
2026-04-27 22:04:43 -0600

    Created Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
 import Navbar from "./Navbar";
+import developer from "./MRHeaderDev.png";
+import psycho from "./MRHeaderPsy.png";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <header>
+        <div className="gallery">
+          <img src={developer} alt="developer" />
+          <img src={psycho} alt="psychologist" />
+        </div>
+      </header>
     </div>
   );
 }
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,14 +4,14 @@ import logo from "./MR-logo.png";
 export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">
+      <a className="navbar-brand" href="/">
         <img src={logo} alt="Mariana Logo" className="logo-bar" />
       </a>
       <button
         className="navbar-toggler"
         type="button"
         data-toggle="collapse"
-        data-target="#navbarSupportedContent"
+        data-target="/navbarSupportedContent"
         aria-controls="navbarSupportedContent"
         aria-expanded="false"
         aria-label="Toggle navigation"
@@ -22,17 +22,17 @@ export default function Navbar() {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
-            <a className="nav-link" href="#">
+            <a className="nav-link" href="/">
               Home <span className="sr-only">(current)</span>
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">
+            <a className="nav-link" href="/">
               About
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">
+            <a className="nav-link" href="/">
               Portfolio
             </a>
           </li>
